0cffe7919a14c92b20ff532fed0b156c8929fe8f
Luo1604 <98147788+Luo1604@users.noreply.github.com>
2023-05-21 17:28:59 +0700

    update code dijkstra

--- final/src/Dijkstra.java
+++ final/src/Dijkstra.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Dijkstra {
     private int V;
-    private List<List<Edge>> adj;
+    private List<List<Node>> adj;
 
     public Dijkstra(int V){
         this.V = V;
@@ -12,31 +12,31 @@ public class Dijkstra {
         }
     }
 
-    public Dijkstra(int V, List<List<Edge>> adj){
+    public Dijkstra(int V, List<List<Node>> adj){
         this.V = V;
         this.adj = adj;
     }
 
     void addEdge(int u, int v, int w) {
-        adj.get(u).add(new Edge(v, w));
-        adj.get(v).add(new Edge(u, w));
+        adj.get(u).add(new Node(v, w));
+        adj.get(v).add(new Node(u, w));
     }
     void dijkstra(int src){
-        PriorityQueue<Edge> queue = new PriorityQueue<>(V, Comparator.comparingInt(o -> o.dest));
+        PriorityQueue<Node> queue = new PriorityQueue<>(V, Comparator.comparingInt(o -> o.id));
         int[] dist = new int[V];
         Arrays.fill(dist, Integer.MAX_VALUE);
 
-        queue.add(new Edge(src, 0));
+        queue.add(new Node(src, 0));
         dist[src] = 0;
 
         while (!queue.isEmpty()) {
-            int u = queue.remove().dest;
+            int u = queue.remove().id;
 
             for (int i = 0; i < adj.get(u).size(); i++) {
-                Edge v = adj.get(u).get(i);
-                if (dist[v.dest] > dist[u] + v.weight) {
-                    dist[v.dest] = dist[u] + v.weight;
-                    queue.add(new Edge(v.dest, dist[v.dest]));
+                Node v = adj.get(u).get(i);
+                if (dist[v.id] > dist[u] + v.distance) {
+                    dist[v.id] = dist[u] + v.distance;
+                    queue.add(new Node(v.id, dist[v.id]));
                 }
             }
         }
@@ -44,51 +44,29 @@ public class Dijkstra {
     }
 
     public static void main(String[] args) {
-        int V = 9;
-        int[][] matrix = {
-            {0, 4, 0, 0, 0, 0, 0, 8, 0},
-            {0, 0, 8, 0, 0, 0, 0, 11, 0},
-            {0, 0, 0, 7, 0, 4, 0, 0, 2},
-            {0, 0, 0, 0, 9, 14, 0, 0, 0},
-            {0, 0, 0, 0, 0, 10, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 2, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 1, 6},
-            {0, 0, 0, 0, 0, 0, 0, 0, 7},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0}
-        };
+        int V = 5;
+        Dijkstra g = new Dijkstra(V);
+        g.addEdge(0, 1, 3);
+        g.addEdge(0, 2, 1);
+        g.addEdge(1, 2, 7);
+        g.addEdge(1, 3, 4);
+        g.addEdge(1, 4, 1);
+        g.addEdge(2, 3, 2);
+        g.addEdge(3, 4, 7);
 
-        List<List<Edge>> adj = adjacencyList(matrix);
-        Dijkstra g = new Dijkstra(V, adj);
-
-//        Dijkstra g = new Dijkstra(V);
-//        g.addEdge(0, 1, 4);
-//        g.addEdge(0, 7, 8);
-//        g.addEdge(1, 2, 8);
-//        g.addEdge(1, 7, 11);
-//        g.addEdge(2, 3, 7);
-//        g.addEdge(2, 8, 2);
-//        g.addEdge(2, 5, 4);
-//        g.addEdge(3, 4, 9);
-//        g.addEdge(3, 5, 14);
-//        g.addEdge(4, 5, 10);
-//        g.addEdge(5, 6, 2);
-//        g.addEdge(6, 7, 1);
-//        g.addEdge(6, 8, 6);
-//        g.addEdge(7, 8, 7);
-//        System.out.println(g.adj.get(0).get(1));
         g.dijkstra(0);
     }
-    public static List<List<Edge>> adjacencyList(int[][] matrix) {
+    public static List<List<Node>> adjacencyList(int[][] matrix) {
         int n = matrix.length;
-        List<List<Edge>> adj = new ArrayList<>();
+        List<List<Node>> adj = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
         }
         for (int u = 0; u < n; u++) {
             for (int v = 0; v < n; v++) {
                 if (matrix[u][v] != 0) {
-                    adj.get(u).add(new Edge(v, matrix[u][v]));
-                    adj.get(v).add(new Edge(u, matrix[u][v]));
+                    adj.get(u).add(new Node(v, matrix[u][v]));
+                    adj.get(v).add(new Node(u, matrix[u][v]));
                 }
             }
         }
@@ -100,23 +78,12 @@ public class Dijkstra {
             System.out.println("Shortest distance from source to vertex " + i + " is " + dist[i]);
         }
     }
-
-    public void printAdj(){
-        for (int u = 0; u < adj.size(); u++) {
-            System.out.print(u + ": ");
-            for (Edge e : adj.get(u)) {
-                System.out.print("(" + e.dest + ", " + e.weight + ") ");
-            }
-            System.out.println();
-        }
-    }
 }
-class Edge {
-    int dest;
-    int weight;
-    public Edge(){}
-    public Edge(int dest, int weight){
-        this.dest = dest;
-        this.weight = weight;
+class Node {
+    int id;
+    int distance;
+    public Node(int id, int distance){
+        this.id = id;
+        this.distance = distance;
     }
 }
